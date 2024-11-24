3b9d166d08b5e2dab249b36c0b1d9f1f375d8c28
Ankit Bishnoi <ankitbishnoi0000029@gmail.com>
2024-11-24 19:16:56 +0530

    24/nov/2024

--- src/Components/All_Categories/Faction_cate/Range.jsx
+++ src/Components/All_Categories/Faction_cate/Range.jsx
@@ -8,10 +8,14 @@ const RenageHandles=(e)=>{
   return (
         <div>      
                 <div className="all-cate-data my-2">
-                <span className='cate_heading mx-3 fs-5 fw-bold'>Filter</span>
-                <div>
-                        <span>{range}</span>
-                    <input step={1000} onChange={RenageHandles} className='range' type="range" min={100} max={100000} />
+                <span className='cate_heading mx-3 fs-5 fw-bold'>Price</span>
+                <div className='range_box' >                       
+                   <span>{range}</span>
+                    <input step={1000} onChange={RenageHandles} className='range' type="range" min={99} max={10099} />
+                    <div>
+                    <span className='min-max'>Min 100</span>
+                    <span className='right-max'>Max 10900</span>
+                    </div>
                 </div>
                 </div>
             </div>
--- src/Components/Product_Detail/Product_Detail.jsx
+++ src/Components/Product_Detail/Product_Detail.jsx
@@ -1,9 +1,16 @@
 import f1 from '/Mens/f1.png'
 import './PRoduct_detail.css'
 import FeaturedContnaier from '../Featured_Products/FeaturedContnaier'
+import { useLocation } from 'react-router-dom';
 
-function Product_Detail({ item }) {
-  console.log(item);
+function Product_Detail() {
+  // Access the location object to get the passed state
+  const location = useLocation();
+  const { item } = location.state || {}; // Destructure `item` from state (if exists)
+
+  if (!item) {
+    return <div>Product not found.</div>; // Handling if no item is passed (edge case)
+  }
 
 
   return (
@@ -12,17 +19,17 @@ function Product_Detail({ item }) {
 
         <div className="col-5">
           <div className="img_box_big">
-            <img height={500} src={f1} alt="" />
+            <img height={500} src={item.img} alt="" />
           </div>
         </div>
         <div className="col-7 ">
           <div className="heading_product_detail py-4">
             <h3>
-              VNEED Women Embroidered Rayon Kurta Pant Set | Kurta set for Women | Ethnic Kurta Set for Women (set of 1) (5XL)
+              {item.pname}  dolor sit amet consectetur adipisicing elit. Laborum, eius.
             </h3>
           </div>
           <div className='product_star py-2'>
-            <span className='hd12b'>Ratings :</span> *****(2 review)
+            <span className='hd12b'>Ratings :</span> 
             *****(2 review)
           </div>
           <div className='product_dec'>
@@ -31,56 +38,37 @@ function Product_Detail({ item }) {
             </p>
           </div>
           <div className='product_brand py-2'>
-            <span className='hd12b'>Brand :</span> <span>Apple</span>
+            <span className='hd12b'>Brand :</span> <span className='text-uppercase' >{item.brand}</span>
           </div>
           <div className='product_size py-3'>
             <nav>
               <span className='hd12b' >Size :</span>  <span className='product_size_box' >S</span> <span className='product_size_box' >M</span> <span className='product_size_box' >L</span> <span className='product_size_box' >XL</span>
             </nav>
-            <div className='product_size py-3'>
-              <nav className='product_color py-3'>
+          </div>
+            <div className='product_size py-2'>
+              <nav className='product_color'>
                 <span className='hd12b' >Colors :</span> <span className='red' ></span> <span className='red bg-success' ></span> <span className='red bg-info' ></span> <span className='red bg-dark' ></span>
               </nav>
             </div>
-            <div className='product_size'>
-              <nav className='product_color '>
-                <span className='hd12b' >Price : </span> <span className='price' > 7000/-</span> <span className='px-2'>50%</span><span className='discount'>14000/-</span>               </nav>
-            </div>
+          <div className='product_price py-2 d-flex text-success fs-4'>
+            Price: {item.price}/- <nav className='px-4 text-primary fs-5'> 50% off <strong className='text-danger fs-6'>{item.price}/-</strong> </nav>
           </div>
-
-          Brand : <strong>Apple</strong>
-        </div>
-        <div className='product_price py-2 d-flex text-success fs-4'>
-          Price: 4000/- <nav className='px-4 text-primary fs-5'> 50% off <strong className='text-danger fs-6'>7000/-</strong> </nav>
-        </div>
-        <div className='product_size py-3'>
-          <nav>
-            SIZE : <span>S</span> <span>M</span> <span>L</span>
-          </nav>
-          <nav className='product_color py-3'>
-            color <span>Red</span> <span>Blue</span> <span>Green</span>
-          </nav>
         </div>
 
-      </div>
-      <div className='col-5'>
-        <div className='p-4 d-flex justify-content-center'>
-          <img className='mx-3' height={60} width={60} src={f1} alt="" />
-          <img className='mx-3' height={60} width={60} src={f1} alt="" />
-          <img className='mx-3' height={60} width={60} src={f1} alt="" />
-          <img className='mx-3' height={60} width={60} src={f1} alt="" />
 
+        <div className='col-5'>
+          <div className='p-4 d-flex justify-content-center'>
+            <img className='mx-3' height={60} width={60} src={f1} alt="" />
+            <img className='mx-3' height={60} width={60} src={f1} alt="" />
+            <img className='mx-3' height={60} width={60} src={f1} alt="" />
+            <img className='mx-3' height={60} width={60} src={f1} alt="" />
+          </div>
         </div>
-      </div>
-      <div className='col-7 align-items-center d-flex justify-content-star' >
-        <div className='prduct_btn_section'>
-
-        </div>
-      </div>
-      <div className='col-7 align-items-center d-flex justify-content-start' >
-        <div className='prduct_btn_section'>
-          <button className="btn btn-success">Buy Now</button>
-          <button className="btn btn-primary">Add to cart</button>
+        <div className='col-7 align-items-center d-flex justify-content-start' >
+          <div className='prduct_btn_section'>
+            <button className="btn btn-success">Buy Now</button>
+            <button className="btn btn-primary">Add to cart</button>
+          </div>
         </div>
       </div>
       <FeaturedContnaier />
