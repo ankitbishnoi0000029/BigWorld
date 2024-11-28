a1eaf243d35f1b017a600dc1fccc7935f4e4f5fe
ankitbishnoi0000029 <84176061+ankitbishnoi0000029@users.noreply.github.com>
2024-11-28 15:15:39 +0530

    Update Range.jsx

--- src/Components/All_Categories/Faction_cate/Range.jsx
+++ src/Components/All_Categories/Faction_cate/Range.jsx
@@ -22,7 +22,7 @@ console.log(minimume,maximume,"this is minmume value get on redux");
         <span className='cate_heading mx-3 fs-5 fw-bold'>Price</span>
         <div className='range_box' >
           <span>{range}</span>
-          <input step={200} onChange={RenageHandles} className='range' type="range" min={minimume} max={maximume} />
+          <input step={100} onChange={RenageHandles} className='range' type="range" min={minimume} max={maximume} />
           <div>
             <span className='min-max'>Min {minimume}</span>
             <span className='right-max'>Max {maximume}</span>
